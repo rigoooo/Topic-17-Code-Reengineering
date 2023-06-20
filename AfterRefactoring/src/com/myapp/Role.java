602176c42f6e4a7be3fe9fd47dfaae3351c04a2f
rigoooo <135738699+rigoooo@users.noreply.github.com>
2023-06-20 12:18:28 +0700

    Add files via upload

---
+++ AfterRefactoring/src/com/myapp/Role.java
@@ -0,0 +1,25 @@
+package com.myapp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Role {
+    private String roleName;
+    private List<UserRole> roleUsers;
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+        roleUsers = new ArrayList<>();
+    }
+
+    public void addRoleUser(UserRole userRole) {
+        roleUsers.add(userRole);
+    }
+
+    public List<UserRole> getRoleUsers() {
+        return roleUsers;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
